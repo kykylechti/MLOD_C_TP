468e4c723cba203f2d409a714d09cf463dda52c0
kykylechti <kyliancarton@gmail.com>
2024-09-16 12:05:37 +0200

    modif

--- Exercism/beer_song.c
+++ Exercism/beer_song.c
@@ -6,14 +6,14 @@
 #define MAX_LINE_COUNT (299)
 
 void recite(uint8_t start_bottles, uint8_t take_down, char **song){
-    for(int i=start_bottles; i>start_bottles-take_down; i--){
-        if(i!=0){
-            sprintf("%d bottles of beer on the wall, %d bottles of beer.", song[3*(start_bottles-i)], i, i);
-            sprintf("Take one down and pass it around, %d bottles of beer on the wall.", song[3*(start_bottles-i)+1], i);
-            song[3*(start_bottles-i)+2]="";
+    for(size_t i=0; i<take_down; i++){
+        if(i!=start_bottles){
+            sprintf(song[3*i], "%d bottles of beer on the wall, %d bottles of beer.", start_bottles-(int)i, start_bottles-(int)i);
+            sprintf(song[3*i+1], "Take one down and pass it around, %d bottles of beer on the wall.", start_bottles-2*(int)i);
+            song[3*i+2]="";
         } else {
-            song[3*start_bottles] = "No more bottles of beer on the wall, no more bottles of beer.";
-            song[3*start_bottles+1] = "Go to the store and buy some more, 99 bottles of beer on the wall.";
+            song[3*i] = "No more bottles of beer on the wall, no more bottles of beer.";
+            song[3*i+1] = "Go to the store and buy some more, 99 bottles of beer on the wall.";
         }
     }
 }
