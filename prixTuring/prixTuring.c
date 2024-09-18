989be18fa67917d16f4695386ab9274dc68277a8
kykylechti <kyliancarton@gmail.com>
2024-09-18 17:27:58 +0200

    lecture et écriture dans un fichier avec calcul du nombre de lignes

--- prixTuring/prixTuring.c
+++ prixTuring/prixTuring.c
@@ -4,14 +4,43 @@
 #include <errno.h>
 #include <assert.h>
 
+//Fonction permettant de calculer la taille d'un fichier
+int nbLigneFichier(char* filepath){
+    FILE* f = fopen(filepath, "r");
+    int tailleFichier = 0;
+    int maxline = 1024; 
+    char* line = malloc(maxline*sizeof(char));
+
+    while(feof(f)==0){
+        fgets(line, maxline, f);
+        tailleFichier++;
+    }
+
+    fclose(f);
+    return tailleFichier;
+}
+
 int main(void){
 
     //Chemin d'accès aux fichiers
     char filename[] = "turingWinners.csv";
 	char outputFilename[] = "out.csv";
 
-    FILE* f = fopen(filename, "r");
+    int maxline = 1024;
+    int tailleFichier=1;
 
+    FILE* f = fopen(filename, "r");
+    FILE* output = fopen(outputFilename, "r");
+    char* line = malloc(maxline*sizeof(char));
+
+    printf(" %s \n", fgets(line, maxline, f));
+    fwrite(line, sizeof(char), maxline, output);
+    
+    
+    printf("Nombre de ligne dans le fichier : %d \n", nbLigneFichier(filename));
+
+    free(line);
     fclose(f);
+    fclose(output);
     return 0;
 }
