0fe491d8439d677fb91cd24f6af603083adc7b01
kykylechti <kyliancarton@gmail.com>
2024-09-18 14:45:42 +0200

    Ajout du TP noté

--- Exercism/beer_song.c
+++ Exercism/beer_song.c
@@ -6,7 +6,7 @@
 #define MAX_LINE_COUNT (299)
 
 void recite(uint8_t start_bottles, uint8_t take_down, char **song){
-    for(size_t i=0; i<take_down; i++){
+    for(size_t i=0; i<(size_t)(take_down-1); i++){
         if(i!=start_bottles){
             sprintf(song[3*i], "%d bottles of beer on the wall, %d bottles of beer.", start_bottles-(int)i, start_bottles-(int)i);
             sprintf(song[3*i+1], "Take one down and pass it around, %d bottles of beer on the wall.", start_bottles-2*(int)i);
