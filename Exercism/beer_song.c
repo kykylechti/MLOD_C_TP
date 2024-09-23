ae443bbf0d88a08b2d6bb6279c45c34a8a4306a4
kykylechti <kyliancarton@gmail.com>
2024-09-23 08:57:14 +0200

    Version finale beer_song

--- Exercism/beer_song.c
+++ Exercism/beer_song.c
@@ -7,7 +7,7 @@
 
 void recite(uint8_t start_bottles, uint8_t take_down, char **song){
     for(int i = start_bottles; i>start_bottles-take_down; i--){
-        if(i>2){
+        if(i>=2){
             sprintf(*song, "%d bottles of beer on the wall, %d bottles of beer.", i, i);
             song++;
             sprintf(*song, "Take one down and pass it around, %d %s of beer on the wall.", i-1, (i-1==1 ? "bottle":"bottles"));
@@ -20,9 +20,9 @@ void recite(uint8_t start_bottles, uint8_t take_down, char **song){
             song++;
         }
         else if(i==0){
-            *song = "No more bottles of beer on the wall, no more bottles of beer.";
+            sprintf(*song, "No more bottles of beer on the wall, no more bottles of beer.");
             song++;
-            *song = "Go to the store and buy some more, 99 bottles of beer on the wall.";
+            sprintf(*song, "Go to the store and buy some more, 99 bottles of beer on the wall.");
             song++;
         }
         song++;
