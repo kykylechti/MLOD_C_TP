6379e19f9f62f950bdaa59cd58606bb1f5b266d3
kykylechti <kyliancarton@gmail.com>
2024-09-23 08:53:17 +0200

    Changement beer_song

--- Exercism/beer_song.c
+++ Exercism/beer_song.c
@@ -6,17 +6,25 @@
 #define MAX_LINE_COUNT (299)
 
 void recite(uint8_t start_bottles, uint8_t take_down, char **song){
-    for(int i=start_bottles; i>start_bottles-take_down; i--){
-        if(i>=2){
-            sprintf(song[3*i], "%d bottles of beer on the wall, %d bottles of beer.", i, i);
-            sprintf(song[3*i+1], "Take one down and pass it around, %d %s of beer on the wall.", i-1, (i-1==1 ? "bottle":"bottles"));
-        } else if(i==1){
-            sprintf(song[3*i], "1 bottle of beer on the wall, 1 bottle of beer.");
-            sprintf(song[3*i+1], "Take it down and pass it around, no more bottles of beer on the wall.");
+    for(int i = start_bottles; i>start_bottles-take_down; i--){
+        if(i>2){
+            sprintf(*song, "%d bottles of beer on the wall, %d bottles of beer.", i, i);
+            song++;
+            sprintf(*song, "Take one down and pass it around, %d %s of beer on the wall.", i-1, (i-1==1 ? "bottle":"bottles"));
+            song++;
+        }
+        else if (i==1){
+            sprintf(*song, "1 bottle of beer on the wall, 1 bottle of beer.");
+            song++;
+            sprintf(*song, "Take it down and pass it around, no more bottles of beer on the wall.");
+            song++;
         }
         else if(i==0){
-            song[3*i] = "No more bottles of beer on the wall, no more bottles of beer.";
-            song[3*i+1] = "Go to the store and buy some more, 99 bottles of beer on the wall.";
+            *song = "No more bottles of beer on the wall, no more bottles of beer.";
+            song++;
+            *song = "Go to the store and buy some more, 99 bottles of beer on the wall.";
+            song++;
         }
+        song++;
     }
 }
